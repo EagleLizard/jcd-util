82a0214a0fbea710226e0dd3aa7f95dd5cbb006d
eaglelizard <whitedtd@gmail.com>
2024-10-16 14:07:47 -0600

    add jcd_image db operations

---
+++ src/jcd-data/jcd-db-v4/jcd-image.ts
@@ -0,0 +1,39 @@
+import { PoolClient } from 'pg';
+import { JcdImageDto, JcdImageDtoType } from '../jcd-dto/jcd-image-dto';
+
+export const JcdImage = {
+  getByPath: getImageByPath,
+  insert: insertImage,
+} as const;
+
+async function getImageByPath(client: PoolClient, opts: {
+  path: string;
+}): Promise<JcdImageDtoType | undefined> {
+  let queryStr = `
+    SELECT * FROM jcd_image ji
+      WHERE ji.path LIKE $1
+  `;
+  let res = await client.query(queryStr, [
+    opts.path,
+  ]);
+  if(res.rows.length < 1) {
+    return;
+  }
+  let jcdImageDto = JcdImageDto.deserialize(res.rows[0]);
+  return jcdImageDto;
+}
+
+async function insertImage(client: PoolClient, opts: {
+  path: string;
+}): Promise<JcdImageDtoType> {
+  let queryStr = `
+    INSERT INTO jcd_image (path)
+      values($1)
+    returning *
+  `;
+  let res = await client.query(queryStr, [
+    opts.path,
+  ]);
+  let jcdImageDto = JcdImageDto.deserialize(res.rows[0]);
+  return jcdImageDto;
+}
