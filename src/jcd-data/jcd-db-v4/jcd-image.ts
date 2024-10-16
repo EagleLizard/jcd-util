6e73076f32cb51cb2b8d641b92ce3e64a6bf5ab4
eaglelizard <whitedtd@gmail.com>
2024-10-16 17:25:31 -0600

    add jcd_project_image db operations

--- src/jcd-data/jcd-db-v4/jcd-image.ts
+++ src/jcd-data/jcd-db-v4/jcd-image.ts
@@ -1,9 +1,15 @@
 import { PoolClient } from 'pg';
 import { JcdImageDto, JcdImageDtoType } from '../jcd-dto/jcd-image-dto';
+import { JcdProjectImageDto, JcdProjectImageDtoType } from '../jcd-dto/jcd-project-image-dto';
 
 export const JcdImage = {
   getByPath: getImageByPath,
   insert: insertImage,
+} as const;
+
+export const JcdProjectImage = {
+  get: getJcdProjectImage,
+  insert: insertJcdProjectImage,
 } as const;
 
 async function getImageByPath(client: PoolClient, opts: {
@@ -36,4 +42,57 @@ async function insertImage(client: PoolClient, opts: {
   ]);
   let jcdImageDto = JcdImageDto.deserialize(res.rows[0]);
   return jcdImageDto;
+}
+
+async function getJcdProjectImage(client: PoolClient, opts: {
+  jcd_project_id: number;
+  jcd_image_id: number;
+  kind: JcdProjectImageDtoType['kind'];
+}): Promise<JcdProjectImageDtoType | undefined> {
+  let querysStr = `
+    SELECT jpi.* FROM jcd_project jp
+      INNER JOIN jcd_project_image jpi
+        ON jp.jcd_project_id = jpi.jcd_project_id
+      INNER JOIN jcd_image ji
+        ON jp.jcd_project_id = ji.jcd_project_id
+    WHERE jpi.jcd_project_id = $1
+      AND jpi.image_id = $2
+      AND jpi.kind = $3
+    ORDER BY jpi.last_modified DESC
+  `;
+  let res = await client.query(querysStr, [
+    opts.jcd_project_id,
+    opts.jcd_image_id,
+    opts.kind,
+  ]);
+  if(res.rows.length < 1) {
+    return;
+  }
+  let jcdProjectImageDto = JcdProjectImageDto.deserialize(res.rows[0]);
+  return jcdProjectImageDto;
+}
+
+async function insertJcdProjectImage(client: PoolClient, opts: {
+  jcd_project_id: number;
+  jcd_image_id: number;
+  kind: JcdProjectImageDtoType['kind'];
+}): Promise<JcdProjectImageDtoType> {
+  let colNames = [
+    'jcd_project_id',
+    'jcd_image_id',
+    'kind',
+  ];
+  let colNums = colNames.map((_, idx) => `$${idx + 1}`);
+  let queryStr = `
+    INSERT INTO jcd_project_image (${colNames.join(', ')})
+      values(${colNums.join(', ')})
+    returning *
+  `;
+  let res = await client.query(queryStr, [
+    opts.jcd_project_id,
+    opts.jcd_image_id,
+    opts.kind,
+  ]);
+  let jcdProjectImageDto = JcdProjectImageDto.deserialize(res.rows[0]);
+  return jcdProjectImageDto;
 }
